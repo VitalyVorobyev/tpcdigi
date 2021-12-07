e3ce32e51a6ba1be33ef77bb75685956d529b1c1
Vitaly Vorobyev <vit.vorobiev@gmail.com>
2021-12-07 18:17:06 +0700

    wip

--- readdata.py
+++ readdata.py
@@ -10,6 +10,22 @@ def group_by_event(data, maxevt=10**7, key='evtn'):
     return {k: data[data[key] == k] for k in unique_events[:maxevt]}
 
 
+def group_by_event_loop(data, maxevt=10**7, key='evtn'):
+    byeve = []
+    curkey = None
+    
+    for evt in data:
+        if curkey == evt[key]:
+            byeve[-1].append(evt)
+        else:
+            byeve.append([evt])
+            curkey = evt[key]
+    return byeve
+    unique_events = np.unique(data[key])
+    maxevt = min(maxevt, unique_events.shape[0])
+    return {k: data[data[key] == k] for k in unique_events[:maxevt]}
+
+
 def get_data(key, datapath, maxevt=10**7):
     g4data = np.load('/'.join([datapath, f'g4hits_gun_{key}_digi.npy']))
     hits = np.load('/'.join([datapath, f'digi_gun_{key}_digi.npy']))
